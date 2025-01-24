9de03c29c5f22f853ce40cbe95f492e5b53029af
Bibash Thapa <crnemo07@gmail.com>
2025-01-24 17:44:38 +0545

    Add assets

--- ydh_frontpage/src/App.jsx
+++ ydh_frontpage/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import { parseAst } from 'vite'
 
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    pass
   )
 }
 
