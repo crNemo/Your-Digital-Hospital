20b27e2b5140ff08b586bcf2723448bef81ee38c
Enzela <133377833+Enzela@users.noreply.github.com>
2025-01-25 01:03:13 +0545

    navbar modif + header addition

--- ydh_frontpage/src/App.jsx
+++ ydh_frontpage/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/ambulance' element={<Ambulance />} /> 
+        <Route path='/ambulance' element={<Ambulance />} />
         <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/my-appointments' element={<MyAppointments />} />
         <Route path='/appointment/:docId' element={<Appointment />} />
