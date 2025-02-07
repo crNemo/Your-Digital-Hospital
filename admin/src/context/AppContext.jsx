c1320eeaa8b4bbaa944ef6a4b8b43b3d28160b5e
BG0091 <gautambishesh100@gmail.com>
2025-02-07 22:05:20 +0545

    ERROR

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -110,6 +110,17 @@ const AppContextProvider = (props) => {
     return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2];
   }, []);
 
+  const calculateAge = (birthDate) => {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDifference = today.getMonth() - birthDateObj.getMonth();
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDateObj.getDate())) {
+      age--;
+    }
+    return age;
+  };
+
   const value = {
     doctors,
     getDoctorsData,
@@ -125,7 +136,8 @@ const AppContextProvider = (props) => {
     loadUserProfileData,
     getBedsData,
     getStreamVideoUserToken,
-    slotDateFormat
+    slotDateFormat,
+    calculateAge
   };
 
   useEffect(() => {
